e56901953ea94ab622fa4f2e17a1bac5e86999fb
Jorge Ejarque <jorge.ejarque@bsc.es>
2023-05-24 16:12:54 +0200

    update spack version and add hecuba

---
+++ packages/py-hecuba/package.py
@@ -0,0 +1,16 @@
+from spack.package import *
+
+
+class PyHecuba(PythonPackage):
+    git = "https://github.com/bsc-dd/hecuba.git"
+    
+    version("main", branch="main")
+    version("1.2.4", tag="v1.2.4")
+
+    depends_on("python@3.6:", type=("build", "link", "run"))
+    depends_on("py-setuptools", type=("build"))
+    depends_on("cmake@3.14:")
+    depends_on("py-numpy")
+    depends_on("cassandra")
+    depends_on("kafka")
+
