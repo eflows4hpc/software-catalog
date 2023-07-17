3e3a89b27d113e6c3e16ba889047a5f6f2f000da
Jorge Ejarque <jorge.ejarque@bsc.es>
2023-07-17 19:03:29 +0200

    update compss and packages

--- packages/compss/package.py
+++ packages/compss/package.py
@@ -34,7 +34,8 @@ class Compss(Package):
     maintainers = ['jorgee', 'compsuperscalar']
 
     version("eflows4hpc", sha256="0251c82cdd4557ed9026f6296f7c475f3d15c3e1fc13edd04c7ff1399328d2b3")
-    version("3.1", sha256="53880443567269faa724d1908a6bc4d82998be3cc93c77386f620c4c8a72e60d", preferred=True)
+    version("3.2", sha256="f32825d7f26bde13cd3699f0aea14da18632b30cf08f113a06ae050033efc837", preferred=True)
+    version("3.1", sha256="53880443567269faa724d1908a6bc4d82998be3cc93c77386f620c4c8a72e60d")
     version("3.0", sha256="3a84e0c6cd84aea155abf08370b1ad5bf5f65c99d4b17d70346c6457e7d0b215")
     version("2.10.1", sha256="0a983a85b53c3a1c82ea63aedf54e353c5627ba25b85c1cda1bbea5a35ba08af")
     version("2.10-riscv", sha256="2170abfc484234b1384a4ec698acfe045ffca56806624e6237f5dab39bfe86bf")
