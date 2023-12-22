29800f9c7092dce8027cd242b69a5ff3ab430df9
Jorge Ejarque <jorge.ejarque@bsc.es>
2023-12-22 18:44:28 +0100

    fix parsoda spack

--- packages/py-parsoda/package.py
+++ packages/py-parsoda/package.py
@@ -27,13 +27,15 @@ class PyParsoda(PythonPackage):
     """ParSoDA: Parallel Social Data Analytics library"""
 
     homepage = "https://github.com/eflows4hpc/parsoda"
-    url = "https://github.com/eflows4hpc/parsoda/archive/refs/tags/1.1.0.tar.gz"
+    url = "https://github.com/eflows4hpc/parsoda/archive/refs/tags/v1.2.0.tar.gz"
 
     # list of GitHub accounts to notify when the package is updated.
     maintainers = ["sv-giampa", "lorisbel"]
 
-    version("1.1.0", sha256="16d5bdab98dea3a176d809e60c43250461371e9bd58fc9be60930b6298907ba4")
+    version("1.2.1", sha256="f0897a95801d7e3d3c4f1d76c3ba93c67b07d2e320d5eca71c1ae501fe145291")
     version("1.2.0", sha256="49e26e51a6b51fc3ae52599b380a6f027c9bafefe8d9e04886e75ce621f3f4d3")
-    
+    version("1.1.0", sha256="287ccf6fb78950217c6f5005fb71afb982e1431a75b012458ff29347082c6e59", url = "https://github.com/eflows4hpc/parsoda/archive/refs/tags/1.1.0.tar.gz")
+    version("1.0.0", sha256="9d3c513f7d5e29fe6a2f286db5549c1fac469dcd3b07ca14684f9362e862a10c", url = "https://github.com/eflows4hpc/parsoda/archive/refs/tags/1.0.0.tar.gz")
+   
     depends_on('python@3.8:', type=('build', 'run'))
     depends_on("py-setuptools", type="build")
